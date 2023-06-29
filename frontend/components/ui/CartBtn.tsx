c17dfd6d8f004e488d7f7577a43506f63e3162ee
Tanuj Patra <tanujpatra228@gmail.com>
2023-06-29 18:33:26 +0530

    Hide cart data if logout

--- frontend/components/ui/CartBtn.tsx
+++ frontend/components/ui/CartBtn.tsx
@@ -3,8 +3,10 @@ import { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { FiShoppingCart } from 'react-icons/fi';
 import CartSidebar from '../CartSidebar';
+import { useSession } from 'next-auth/react';
 
 const CartBtn = () => {
+    const { data: session } = useSession();
     const { cart } = useSelector((state: any) => state);
     const [cartIsOpen, setCartIsOpen] = useState(false);
     const ping = useRef<HTMLSpanElement>();
@@ -24,7 +26,7 @@ const CartBtn = () => {
         <>
             <button className='rounded-full px-2 py-1 flex justify-center items-center relative text-xl' onClick={() => setCartIsOpen(true)}>
                 <FiShoppingCart />
-                {!isNaN(totalQty) && totalQty > 0 && (
+                {session && !isNaN(totalQty) && totalQty > 0 && (
                     <>
                         <span ref={ping} className={`bg-red-700 rounded-full min-w-[20px] min-h-[20px] px-1 absolute top-1 right-0 text-xs flex justify-center items-center`} />
                         <span className='bg-red-700 rounded-full min-w-[20px] min-h-[20px] px-1 absolute top-1 right-0 text-xs flex justify-center items-center'>{totalQty}</span>
