98b087f293a78ca2f73cdee11e385bacfa25868e
Tanuj Patra <tanujpatra228@gmail.com>
2023-07-05 15:21:02 +0530

    Add Shipping option

--- frontend/app/cart/page.tsx
+++ frontend/app/cart/page.tsx
@@ -1,11 +1,17 @@
 'use client'
+import { useState } from "react";
 import { useAppSelector } from "@/utils/hooks";
 import EmptyResource from "@/components/ui/EmptyResource";
 import Link from "next/link";
 import { BsArrowLeft } from "react-icons/bs";
 import CartPageTable from "@/components/CartPageTable";
+import { Switch } from "@headlessui/react";
+import { FcFlashOn } from "react-icons/fc"
+
+const SHIPPING_CHARGES = 50;
 
 const CartPage = () => {
+    const [isFastShipping, setIsFastShipping] = useState(false);
     const { cart } = useAppSelector((state: any) => state);
     const products = cart.data.products || [];
 
@@ -22,6 +28,9 @@ const CartPage = () => {
         }
 
         return totalAmount;
+    }
+    const calcShipping = (amount = 0) => {
+        return isFastShipping ? amount + SHIPPING_CHARGES : amount;
     }
     return (
         <>
@@ -50,7 +59,7 @@ const CartPage = () => {
                                         <CartPageTable cartId={cart.data._id} products={products} />
 
                                         <div className="flex justify-between mt-10 mb-5">
-                                            <Link href="/products" className="font-semibold text-indigo-600 text-sm mt-10 flex items-center gap-2">
+                                            <Link href="/products" className="font-semibold text-blue-800 text-sm mt-10 flex items-center gap-2">
                                                 <BsArrowLeft />
                                                 Continue Shopping
                                             </Link>
@@ -60,20 +69,41 @@ const CartPage = () => {
                                     <div id="summary" className="w-1/4 px-8 py-10">
                                         <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
                                         <div className="flex justify-between mt-10 mb-5">
-                                            <span className="font-semibold text-sm uppercase">Total Items: {products.length}</span>
-                                            <span className="font-semibold text-sm">${getCartTotal()}</span>
+                                            <span className="font-semibold text-sm capitalize">Total Items: {products.length}</span>
+                                            <span className="font-semibold text-sm">₹{getCartTotal()}</span>
                                         </div>
-                                        <div className="py-10">
-                                            <label htmlFor="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Promo Code</label>
-                                            <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full" />
+                                        <div className="flex justify-between mb-5">
+                                            <div className="font-semibold text-sm capitalize flex items-center gap-4">
+                                                <div className="w-28 h-[18px] overflow-hidden">
+                                                    <div className={`transition delay-100 ${isFastShipping ? '-translate-y-4' : 'translate-y-0'}`}>
+                                                        <p className={`transition delay-100 ${isFastShipping ? 'text-white' : 'text-gray-800'} leading-4`}>Regular Shipping</p>
+                                                        <p className={`transition delay-100 ${!isFastShipping ? 'text-white' : 'text-gray-800'} leading-4 flex items-center`}>Fast Shipping <FcFlashOn className="text-lg" /></p>
+                                                    </div>
+                                                </div>
+                                                <div className="flex items-center gap-2">
+                                                    <Switch
+                                                        checked={isFastShipping}
+                                                        onChange={setIsFastShipping}
+                                                        className={`${isFastShipping ? 'bg-blue-800' : 'bg-gray-200'
+                                                            } relative inline-flex h-6 w-11 items-center rounded-full`}
+                                                    >
+                                                        <span className="sr-only">Enable Fast Shipping</span>
+                                                        <span
+                                                            className={`${isFastShipping ? 'translate-x-6' : 'translate-x-1'
+                                                                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                                                        />
+                                                    </Switch>
+                                                </div>
+                                            </div>
+                                            <span className="font-semibold text-sm">₹{isFastShipping ? `${getCartTotal()} + ₹${SHIPPING_CHARGES}` : getCartTotal()}</span>
                                         </div>
-                                        <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Apply</button>
+
                                         <div className="border-t mt-8">
                                             <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-                                                <span>Total cost</span>
-                                                <span>₹{getCartTotal()}</span>
+                                                <span className="capitalize">Total cost</span>
+                                                <span>₹{(getCartTotal() + calcShipping(getCartTotal()))}</span>
                                             </div>
-                                            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
+                                            <button className="bg-blue-800 font-semibold hover:bg-blue-900 py-3 text-sm text-white uppercase w-full">Checkout</button>
                                         </div>
                                     </div>
                                 </>
--- frontend/redux/slice/cartMethods.ts
+++ frontend/redux/slice/cartMethods.ts
@@ -29,8 +29,6 @@ type RemoveRequestType = {
     size: string;
 };
 export const removeFromCart = createAsyncThunk('removeFromCart', async ({ cartId, id, size }: RemoveRequestType) => {
-    console.log('cartId', cartId);
-
     const response = await axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/cart/remove-product`, { cartId, id, size });
 
     if (response.status === 200) {
